049fea394655fb099d35f66bb0d637afba54b393
Shivam <shivam0sharma85@gmail.com>
2021-05-10 18:29:38 +0530

    publish to github

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from uploader.models import Videos
+
+# def home(request):
+#     videos = Videos.objects.all()
+#     return render(request, 'home_page.html', {'videos': videos[::-1]})
+
+def home(request):
+    videos = Videos.objects.all()
+    return render(request, 'home_page.html', {'videos': videos[::-1]})
+
